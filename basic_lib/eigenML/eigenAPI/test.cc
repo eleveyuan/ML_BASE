a4f6bea6634772d3d9a9a2301fd6713b90b3a651
eleveyuan <eleveyuan@gmail.com>
2021-10-23 22:30:13 +0800

    base knowledge of eigen

--- basic_lib/eigenML/eigenAPI/test.cc
+++ basic_lib/eigenML/eigenAPI/test.cc
@@ -1,14 +1,63 @@
 #include <iostream>
+#include <vector>
+
 #include <Eigen/Dense>
  
 using Eigen::MatrixXd;
- 
+
+typedef Eigen::Matrix<float, 3, 3> Matrix33f;
+typedef Eigen::Matrix<float, 3, 1> Vector3f;
+typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> DMatrix;  // dynamic matrix
+
+
+void initial() {
+    Matrix33f a;
+    Vector3f v;
+    DMatrix m(10, 5);
+
+    a = Matrix33f::Identity(); 
+    a << 1, 2, 3, 4, 5, 6, 7, 8, 9;  // comma-initializer syntax
+
+    // change an element of matrix directly
+    a(0, 0) = 4;
+
+    v = Vector3f::Random();
+
+    std::cout << a << std::endl;
+    std::cout << v << std::endl;
+}
+
+void initialByMap() {
+    int data[] = {1,2,3,4};
+    Eigen::Map<Eigen::RowVectorXi> v(data,4);
+    std::vector<float> data1 = {1,2,3,4,5,6,7,8,9};
+    Eigen::Map<Matrix33f> a(data1.data());
+
+    std::cout << v << std::endl;
+    std::cout << a << std::endl;
+}
+
+void calc() {
+    Matrix33f a, b;
+    a = Matrix33f::Random();
+    b = Matrix33f::Random();
+
+    std::cout << a+b << std::endl;  // eigen overload operators, like +/-/*
+    std::cout << a.array() * b.array() << std::endl;  // element-wise multiplication
+    std::cout << a*b << std::endl;   // matrix multiplication
+
+}
+
 int main()
 {
-  MatrixXd m(2,2);
-  m(0,0) = 3;
-  m(1,0) = 2.5;
-  m(0,1) = -1;
-  m(1,1) = m(1,0) + m(0,1);
-  std::cout << m << std::endl;
+    std::cout << "intial: " << std::endl;
+    initial();
+
+    std::cout << "intial by Eigen::Map: " << std::endl;
+    initialByMap();
+
+    std::cout << "calculate use Eigen: " << std::endl;
+    calc();
+
+    return 0;
 }
--- basic_lib/eigenML/readme.md
+++ basic_lib/eigenML/readme.md
@@ -1,6 +1,6 @@
 Eigen is a general-purpose linear algebra C++ library.
 
-# use cmake 
+# use cmake import eigen
 
 https://eigen.tuxfamily.org/dox/TopicCMakeGuide.html
 
@@ -13,3 +13,46 @@ step 2: use cmake build eigen
 step 3: cmake your project 
 
 step 4: you are happy to use it
+
+
+# Eigen 
+In eigen, all matrices and vectors are objects of the Matrix template class 
+
+## intial
+
+use typedef define a (3, 3) shape of matrix 
+``` c++
+typedef Eigen::Matrix<float, 3, 3> Matrix33f;
+```
+
+use **"comma-initializer"** initial matrix
+``` c++
+a << 1, 2, 3, 4, 5, 6, 7, 8, 9;   
+```
+
+change an element of matrix directly
+``` c++
+a(0, 0) = 4;
+```
+
+use Eigen::Map to wrap a c/c++ raw array/vector
+``` c++
+int data[] = {1,2,3,4};
+Eigen::Map<Eigen::RowVectorXi> v(data,4);
+std::vector<float> data1 = {1,2,3,4,5,6,7,8,9};
+Eigen::Map<Matrix33f> a(data1.data());
+```
+
+
+## calcualte
+
+``` c++
+Matrix33f a, b;
+a = Matrix33f::Random();
+b = Matrix33f::Random();
+
+std::cout << a+b << std::endl;  // eigen overload operators, like +/-/*
+std::cout << a.array() * b.array() << std::endl;  // element-wise multiplication
+std::cout << a*b << std::endl;   // matrix multiplication
+```
+
